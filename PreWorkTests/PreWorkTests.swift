3de90da6f7d5e0738b468b7ce4baa2aabb8fb0bc
juliannea <julianneaguilar74@gmail.com>
2025-01-16 15:02:29 -0500

    Initial commit

---
+++ PreWorkTests/PreWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreWorkTests.swift
+//  PreWorkTests
+//
+//  Created by Julianne Aguilar on 1/16/25.
+//
+
+import Testing
+@testable import PreWork
+
+struct PreWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
